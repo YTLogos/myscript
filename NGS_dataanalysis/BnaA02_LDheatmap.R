58e28fd285ad2a29a4fb781944029fec85922ebb
YTLogos <1051927088@qq.com>
2018-06-26 12:57:12 +0800

    change the script

--- NGS_dataanalysis/BnaA02_LDheatmap.R
+++ NGS_dataanalysis/BnaA02_LDheatmap.R
@@ -5,7 +5,8 @@ BnaA02_snp <- read.table("BnaA02_snp.txt", header = TRUE, check.names = FALSE)
 BnaA02_dist <- read.table("BnaA02_dist.txt", header = FALSE)
 BnaA02_dist <- as.numeric(BnaA02_dist)
 for (i in 1:ncol(BnaA02_snp)){
-  BnaA02_snp[,1] <- as.genotype(BnaA02_snp[,1])
+  BnaA02_snp[,i] <- as.genotype(BnaA02_snp[,i])
 }
+MyHeatmap <- LDheatmap(BnaA02_snp, genetic.distances = BnaA02_dist,color = grey.colors(20))
 rgb.palette <- colorRampPalette(rev(c("white","red")), space="rgb")
 LDheatmap(MyHeatmap,color = rgb.palette(100), flip = TRUE)
